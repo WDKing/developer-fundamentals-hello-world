f9c698b20ef67eadbfc1178b3eb6b09b2b4f0fd7
William King <Will.David.King@gmail.com>
2018-01-25 07:18:08 -0600

    Completion of practicals exercise, with personal modifications.

--- app/src/main/java/com/practicals/wdking/adfhelloworld/MainActivity.java
+++ app/src/main/java/com/practicals/wdking/adfhelloworld/MainActivity.java
@@ -5,9 +5,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int mCount = 0;
+    private TextView mShowCount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +19,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Log.d("MainActivity", "Hello World");
+
+        mShowCount = (TextView) findViewById(R.id.show_count);
     }
 
     public void showToast(View view) {
@@ -22,8 +28,17 @@ public class MainActivity extends AppCompatActivity {
         Context context = getApplicationContext();
         Toast toast = Toast.makeText(context, R.string.toast_message, Toast. LENGTH_LONG);
         toast.show();
+
+        mCount = 0;
+        if(mShowCount != null)
+            mShowCount.setText(Integer.toString(mCount));
     }
 
     public void countUp(View view) {
+
+        mCount++;
+
+        if (mShowCount != null)
+            mShowCount.setText(Integer.toString(mCount));
     }
 }
